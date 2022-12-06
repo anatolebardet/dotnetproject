8832c06caa6d67c28db74dafedd3be495d2fe96c
CuriouStudent <john.moran@reseau.eseo.fr>
2022-12-06 13:54:13 +0100

    commit businesses

---
+++ VideoTheque/VideoTheque/Businesses/BluRay/IBlueRaysBusiness.cs
@@ -0,0 +1,17 @@
+﻿using VideoTheque.DTOs;
+
+namespace VideoTheque.Businesses.BluRays
+{
+    public interface IBluRaysBusiness
+    {
+        Task<List<BluRayDto>> GetBluRays();
+
+        BluRayDto GetBluRay(int id);
+
+        BluRayDto InsertBluRay(BluRayDto BluRay);
+
+        void UpdateBluRay(int id, BluRayDto BluRay);
+
+        void DeleteBluRay(int id);
+    }
+}
---
+++ VideoTheque/VideoTheque/Businesses/Host/IHostsBusiness.cs
@@ -0,0 +1,17 @@
+﻿using VideoTheque.DTOs;
+
+namespace VideoTheque.Businesses.Hosts
+{
+    public interface IHostsBusiness
+    {
+        Task<List<HostDto>> GetHosts();
+
+        HostDto GetHost(int id);
+
+        HostDto InsertHost(HostDto Host);
+
+        void UpdateHost(int id, HostDto Host);
+
+        void DeleteHost(int id);
+    }
+}
