83116aa97b5915ba792205cc2cd1a3f1f66ba802
bardetanatole@gmail.com <bardetanatole@gmail.com>
2022-12-06 14:17:01 +0100

    names

---
+++ VideoTheque/VideoTheque/Businesses/Films/IFilmsBusiness.cs
@@ -0,0 +1,17 @@
+﻿using VideoTheque.DTOs;
+
+namespace VideoTheque.Businesses.Films
+{
+    public interface IFilmsBusiness
+    {
+        Task<List<FilmDto>> GetFilms();
+
+        FilmDto GetFilm(int id);
+
+        FilmDto InsertFilm(FilmDto Film);
+
+        void UpdateFilm(int id, FilmDto Film);
+
+        void DeleteFilm(int id);
+    }
+}
