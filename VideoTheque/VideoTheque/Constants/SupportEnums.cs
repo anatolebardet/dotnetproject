399c83dec36fb5d9b6043692c7060fe3e5fa8fc7
bardetanatole@gmail.com <bardetanatole@gmail.com>
2022-12-13 10:35:49 +0100

    pushed

--- VideoTheque/VideoTheque/Constants/SupportEnums.cs
+++ VideoTheque/VideoTheque/Constants/SupportEnums.cs
@@ -4,11 +4,7 @@ namespace VideoTheque.Constants
     public enum SupportEnums
 
     {
-<<<<<<< HEAD
-        Bluray = 1;
-=======
         Bluray = 1
->>>>>>> abaaa31f35aef25d508217628a3b3a975d0d0b8d
     }
 
     
